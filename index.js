bc68be7be67e86d35ef1fc3f953381dfc267e1b5
Emily <emily.madsen03@gmail.com>
2024-07-12 00:00:44 -0700

    landing page and routing with ndoe

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const { readFile } = require('fs');
+const path = require('path');
+
+const app = express();
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route for the home page
+app.get('/', (request, response) => {
+    readFile(path.join(__dirname, 'front-end', 'landing-page.html'), 'utf8', (err, html) => {        if (err) {
+            console.error('Error reading landing-page.html:', err);
+            response.status(500).send('Sorry, something went wrong!');
+            return;
+        } 
+        response.send(html);
+    });
+});
+
+app.get('/about-page', (request, response) => {
+    readFile(path.join(__dirname, 'front-end','about-page.html'), 'utf8', (err, html) => {
+        if (err) {
+            console.error('Error reading about-page.html:', err);
+            response.status(500).send('Sorry, something went wrong!');
+            return;
+        }
+        response.send(html);
+    });
+});
+
+app.listen(process.env.PORT || 4001, () => {
+    console.log('App available on http://localhost:4001');
+});
