cd8474c3c581684196aa01d43baa07e084c5ced7
Emily <emily.madsen03@gmail.com>
2024-07-16 23:27:48 -0700

    commit for my contact page

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const path = require('path');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'front-end', 'public')));
+app.use(express.static(path.join(__dirname, 'front-end', 'static')));
 
 
 app.get('/', (request, response) => {
@@ -32,6 +33,28 @@ app.get('/experience-page', (request, response) => {
     readFile(path.join(__dirname, 'front-end','experience-page.html'), 'utf8', (err, html) => {
         if (err) {
             console.error('Error reading experience-page.html:', err);
+            response.status(500).send('Sorry, something went wrong!');
+            return;
+        }
+        response.send(html);
+    });
+});
+
+app.get('/resume', (request, response) => {
+    readFile(path.join(__dirname, 'front-end','resume.html'), 'utf8', (err, html) => {
+        if (err) {
+            console.error('Error reading resume.html:', err);
+            response.status(500).send('Sorry, something went wrong!');
+            return;
+        }
+        response.send(html);
+    });
+});
+
+app.get('/contact-page', (request, response) => {
+    readFile(path.join(__dirname, 'front-end','contact-page.html'), 'utf8', (err, html) => {
+        if (err) {
+            console.error('Error reading contact-page.html:', err);
             response.status(500).send('Sorry, something went wrong!');
             return;
         }
